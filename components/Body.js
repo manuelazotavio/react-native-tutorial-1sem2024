e4eeeaa5d4a1005f12fae193915e8ae84a23b98c
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-03-14 14:35:44 -0300

    change api fetch

--- components/Body.js
+++ components/Body.js
@@ -3,44 +3,44 @@ import { useEffect } from 'react'
 import H1 from './ui/H1'
 import CardUser from './CardUser'
 
-const users = [
-  {
-    id: 1,
-    name: "Renan Cavichi",
-    email: "renancavichi@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/4259630?v=4"
-  },
-  {
-    id: 2,
-    name: "Maria",
-    email: "maria@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/114303361?v=4"
-  },
-  {
-    id: 3,
-    name: "Manuel",
-    email: "manuel@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/4061891?v=4"
-  },
-  {
-    id: 4,
-    name: "Camila",
-    email: "camila@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/4061891?v=4"
-  },
-  {
-    id: 5,
-    name: "Renan Cavichi",
-    email: "renancavichi@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/4259630?v=4"
-  },
-  {
-    id: 6,
-    name: "Maria",
-    email: "maria@gmail.com",
-    avatar:"https://avatars.githubusercontent.com/u/114303361?v=4"
-  },
-]
+// const users = [
+//   {
+//     id: 1,
+//     name: "Renan Cavichi",
+//     email: "renancavichi@gmail.com",
+//     avatar:"https://avatars.githubusercontent.com/u/4259630?v=4"
+//   },
+//   {
+//     id: 2,
+//     name: "Maria",
+//     email: "maria@gmail.com",
+//     avatar:"https://avatars.githubusercontent.com/u/114303361?v=4"
+//   },
+//   {
+//     id: 3,
+//     name: "Manuel",
+//     email: "manuel@gmail.com",
+//     avatar:"https://avatars.githubusercontent.com/u/4061891?v=4"
+//   },
+//   {
+//     id: 4,
+//     name: "Camila",
+//     email: "camila@gmail.com",
+//     avatar:"https://avatars.githubusercontent.com/u/4061891?v=4"
+//   },
+//   {
+//     id: 5,
+//     name: "Renan Cavichi",
+//     email: "renancavichi@gmail.com",
+//     avatar:"https://avatars.githubusercontent.com/u/4259630?v=4"
+//   },
+//   {
+//     id: 6,
+//     name: "Maria",
+//     email: "maria@gmail.com",
+//     avatar:"https://avatars.githubusercontent.com/u/114303361?v=4"
+//   },
+// ]
 
 
 
@@ -48,7 +48,7 @@ const Body = () => {
 
   const getUsers = async () =>{
     try{
-      const result = await fetch('https://brasilapi.com.br/api/cvm/corretoras/v1')
+      const result = await fetch('https://backend-api-express-1sem2024-jf2z.onrender.com/user')
       const data = await result.json()
       console.log(data[0].nome_social)
     } catch (error){
